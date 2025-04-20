7a68ec5e7e4de103889e07b49099e706b83724c9
Esteban Morales <esrimova@gmail.com>
2025-04-20 14:58:28 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductoApiController;
 
 Route::get('/productos/proximos-a-vencer', [ProductoApiController::class, 'productosProximosAVencer']);
